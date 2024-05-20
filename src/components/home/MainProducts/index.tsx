942ab5dcbf42e0e11ad0e7b6881ed4f333010cef
Sebastián Trujillo <juanstt99@gmail.com>
2024-05-20 16:48:47 -0500

    feat(add-shopify-api)

--- src/components/home/MainProducts/index.tsx
+++ src/components/home/MainProducts/index.tsx
@@ -1,8 +1,22 @@
 import { FunctionComponent } from 'react';
+const getProducts = async () => {
+  const response = await fetch(
+    `${process.env.SHOPIFY_HOST_NAME}/admin/api/2023-10/products.json`,
+    {
+      headers: new Headers({
+        'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || '',
+      }),
+    }
+  );
 
+  return await response.json();
+};
 interface MainProductsProps {}
 
-export const MainProducts: FunctionComponent<MainProductsProps> = () => {
+export const MainProducts: FunctionComponent<MainProductsProps> = async () => {
+  const products = await getProducts();
+
+  console.log(products);
   return (
     <section>
       <h1>MainProducts</h1>
