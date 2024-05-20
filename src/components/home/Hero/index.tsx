5e811868f782e98886c2071acd20b00a24f80e48
Sebastián Trujillo <juanstt99@gmail.com>
2024-05-20 11:49:46 -0500

    feat(into-css-module)

--- src/components/home/Hero/index.tsx
+++ src/components/home/Hero/index.tsx
@@ -1,11 +1,14 @@
 import { FunctionComponent } from 'react';
 
+import styles from '@/components/home/Hero/Hero.module.css';
+
 interface HeroProps {}
 
 export const Hero: FunctionComponent<HeroProps> = () => {
   return (
-    <section>
-      <h1>Hero</h1>
+    <section className={styles.Hero}>
+      <h1>Future world</h1>
+      <h2>Empowering Your Tomorrow, Today!</h2>
     </section>
   );
 };
